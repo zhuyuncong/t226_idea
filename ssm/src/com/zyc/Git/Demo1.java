d4f7f16e1aa56638ecc48b1220da273dc8a61a88
zhuyuncong <2191285001@qq.com>
2019-12-15 01:51:02 +0800

    本地仓库接收

---
+++ ssm/src/com/zyc/Git/Demo1.java
@@ -0,0 +1,13 @@
+package com.zyc.Git;
+
+/**
+ * @author 简单定义存在
+ * @site https://i.csdn.net/#/uc/profile
+ * @company xxx公司
+ * @create  2019-12-15 1:47
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea 操作 git");
+    }
+}
